a6213436088051d15eb141f0c3b075d4a15a774e
Guzey <Guzeyst@gmail.com>
2022-02-11 23:41:26 +0300

    Start version

---
+++ app/src/main/java/ru/guzeyst/a65appstest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.guzeyst.a65appstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
